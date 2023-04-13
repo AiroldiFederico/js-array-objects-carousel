7ee1830abf39bc1fbe53b403b85baf3b6e19ba62
Federico Airoldi <airoldibusiness@gmail.com>
2023-04-13 16:17:20 +0200

    FASE 2 - importo array oggetti e sposto parti comuni

--- asset/js/main.js
+++ asset/js/main.js
@@ -29,7 +29,7 @@ Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 
 
 // array immagini
-let images = [
+let imagesold = [
   './asset/img/01.webp',
   './asset/img/02.webp',
   './asset/img/03.webp',
@@ -37,10 +37,7 @@ let images = [
   './asset/img/05.webp',
 ]
 
-// dichiaro variabili che puntano alle freccie
-// freccia sopra e sotto
-let prev = document.querySelector('.prev');
-let next = document.querySelector('.next');
+
 
 //dichiaro variabile per l'index
 let index = 0;
@@ -51,11 +48,11 @@ let img = document.querySelector(".image");
 next.addEventListener('click', function(){
 
   //uso l'indice array per l'attributo src
-  img.setAttribute('src', images[index]);
+  img.setAttribute('src', imagesold[index]);
   index++;
 
   //setto l'indice a 0 quando finisce l'array
-  if (index >= images.length) {
+  if (index >= imagesold.length) {
       index = 0;
   }
 
@@ -65,12 +62,47 @@ next.addEventListener('click', function(){
 prev.addEventListener('click', function(){
 
   //uso l'indice array per l'attributo src
-  img.setAttribute('src', images[index]);
+  img.setAttribute('src', imagesold[index]);
   index--;
 
   //setto l'indice a 0 quando finisce l'array
   if (index < 0) {
-      index = images.length - 1;
+      index = imagesold.length - 1;
   }
 
 });
+
+
+
+/* NUOVA LOGICA ----------------------------------------------*/
+
+
+const images = [
+  {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+  }, {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  }, {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  }, {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  }, {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+  }
+];
+
+
+// dichiaro variabili che puntano alle freccie
+// freccia sopra e sotto
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
